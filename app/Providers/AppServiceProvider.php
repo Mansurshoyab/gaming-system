cdc9a510e19f2435d9fd1ededb45972a95ab329c
shoyab <abumansurshoyab@gmail.com>
2024-11-12 01:00:49 +0600

    backend layout registered

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\View\Layouts\Backend as BackendLayout;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Layouts
+        Blade::component('backend-layout', BackendLayout::class);
     }
 }
