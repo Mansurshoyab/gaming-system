fa20590d58ccba911eb79e8de8c5b722d1dec0de
shoyab <abumansurshoyab@gmail.com>
2024-11-12 01:45:44 +0600

    backend partials booted

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use App\View\Layouts\Backend as BackendLayout;
+use App\View\Partials\Backend\Sidebar as BackendSidebar;
+use App\View\Partials\Backend\Navbar as BackendNavbar;
+use App\View\Partials\Backend\Breadcrumb as BackendBreadcrumb;
+use App\View\Partials\Backend\Footer as BackendFooter;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +27,11 @@ class AppServiceProvider extends ServiceProvider
     {
         // Layouts
         Blade::component('backend-layout', BackendLayout::class);
+
+        // Backend Partials
+        Blade::component('backend-sidebar', BackendSidebar::class);
+        Blade::component('backend-navbar', BackendNavbar::class);
+        Blade::component('backend-breadcrumb', BackendBreadcrumb::class);
+        Blade::component('backend-footer', BackendFooter::class);
     }
 }
