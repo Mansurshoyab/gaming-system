587b6f3973259afc320761839892173f504cc463
shoyab <abumansurshoyab@gmail.com>
2024-11-12 00:37:15 +0600

    status enum created

---
+++ app/Enums/GlobalUsage/Status.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Enums\GlobalUsage;
+
+use Spatie\Enum\Laravel\Enum;
+
+final class Status extends Enum
+{
+    const PENDING = "pending";
+    const ENABLE = "enable";
+    const DISABLE = "disable";
+
+    /**
+     * Get all status values
+     *
+     * @return array
+     */
+    public static function fetch()
+    {
+        return [
+            self::PENDING,
+            self::ENABLE,
+            self::DISABLE,
+        ];
+    }
+}
