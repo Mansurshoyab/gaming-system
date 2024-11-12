151d448703be18dbf02d4bd994fbbcd8c659f587
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 20:05:02 +0600

    notifications schema designed

--- database/migrations/0001_01_01_000011_create_notifications_table.php
+++ database/migrations/0001_01_01_000011_create_notifications_table.php
@@ -4,16 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('member_id')->nullable();
+            $table->string('icon', 25)->nullable();
+            $table->string('title', 50)->nullable();
+            $table->string('description', 250)->nullable();
+            $table->boolean('read')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('tickets', function (Blueprint $table) {
@@ -30,8 +35,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('notifications');
         Schema::dropIfExists('tickets');
         Schema::dropIfExists('messages');
