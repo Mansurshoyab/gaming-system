c9b97a880ef4545170387c99ec9b3046ffd256b0
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 13:26:13 +0600

    games migration minimized

--- database/migrations/2024_11_12_063911_create_games_table.php
+++ database/migrations/2024_11_12_063911_create_games_table.php
@@ -4,13 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
@@ -20,8 +23,8 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
+        Schema::dropIfExists('genres');
         Schema::dropIfExists('games');
     }
 };
