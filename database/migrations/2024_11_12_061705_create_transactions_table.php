10a171eab3827a7f4ae24ae9520c63cdab377680
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 13:09:46 +0600

    transaction migration minimized

--- database/migrations/2024_11_12_061705_create_transactions_table.php
+++ database/migrations/2024_11_12_061705_create_transactions_table.php
@@ -4,13 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('withdraws', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
@@ -20,8 +33,10 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
+        Schema::dropIfExists('accounts');
+        Schema::dropIfExists('deposits');
+        Schema::dropIfExists('withdraws');
         Schema::dropIfExists('transactions');
     }
 };
