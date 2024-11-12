248b42425265d560e784bbceba4a4615c2e0bf5b
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 19:25:47 +0600

    wallets schema designed

--- database/migrations/0001_01_01_000010_create_wallets_table.php
+++ database/migrations/0001_01_01_000010_create_wallets_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('member_id')->nullable();
+            $table->decimal('amount', 10, 2)->default(0);
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('payouts', function (Blueprint $table) {
@@ -25,8 +27,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('wallets');
         Schema::dropIfExists('payouts');
     }
