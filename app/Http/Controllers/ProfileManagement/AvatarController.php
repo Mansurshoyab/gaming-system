f956bf60068453f8fabf6746bc9b93df0a56cb15
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 13:00:41 +0600

    avatar model created

---
+++ app/Http/Controllers/ProfileManagement/AvatarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\ProfileManagement;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProfileManagement\Avatar;
+use Illuminate\Http\Request;
+
+class AvatarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Avatar $avatar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Avatar $avatar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Avatar $avatar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Avatar $avatar)
+    {
+        //
+    }
+}
---
+++ app/Models/ProfileManagement/Avatar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\ProfileManagement;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Avatar extends Model
+{
+    //
+}
