b2e1341b6a415efe269d015094725441a142a2e1
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 13:42:49 +0600

    systems migration minified

--- database/migrations/2024_11_12_051256_create_systems_table.php
+++ database/migrations/2024_11_12_051256_create_systems_table.php
@@ -11,7 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable');
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+
         Schema::create('systems', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::create('modules', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +38,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('personal_access_tokens');
         Schema::dropIfExists('systems');
+        Schema::dropIfExists('modules');
     }
 };
