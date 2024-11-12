755b3e6018f6cbe300334089ed23c5ed4c89b274
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 17:45:33 +0600

    settings schema designed

--- database/migrations/0001_01_01_000003_create_settings_table.php
+++ database/migrations/0001_01_01_000003_create_settings_table.php
@@ -1,19 +1,24 @@
 <?php
 
+use App\Enums\GlobalUsage\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('icon')->nullable();
+            $table->string('label', 25)->unique();
+            $table->string('description', 250)->nullable();
+            $table->string('key', 25)->unique();
+            $table->enum('status', Status::fetch())->default(Status::PENDING);
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('features', function (Blueprint $table) {
@@ -25,8 +30,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('settings');
         Schema::dropIfExists('features');
     }
