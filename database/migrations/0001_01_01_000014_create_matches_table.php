af173907e2b9761b29c8d07b440db6ef43c4a9a1
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 23:36:59 +0600

    matches schema designed

--- database/migrations/0001_01_01_000014_create_matches_table.php
+++ database/migrations/0001_01_01_000014_create_matches_table.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('matches', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('member_id')->unsigned();
+            $table->foreignId('game_id')->unsigned();
+            $table->timestamp('start')->nullable();
+            $table->timestamp('end')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('rounds', function (Blueprint $table) {
@@ -35,8 +38,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('matches');
         Schema::dropIfExists('rounds');
         Schema::dropIfExists('bets');
