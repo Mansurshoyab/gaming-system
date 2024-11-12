d56c30fc28386290e4980a56b407c2a4cd7188ec
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 18:34:18 +0600

    members schema designed

--- database/migrations/0001_01_01_000007_create_users_table.php
+++ database/migrations/0001_01_01_000007_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\UserManagement\Approval;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,21 +12,48 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname', 50);
+            $table->string('lastname', 50)->nullable();
+            $table->string('username', 15)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 19)->unique();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->string('otp_code', 6)->unique()->nullable();
+            $table->timestamp('otp_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role_id')->default(10);
+            $table->enum('status', Approval::fetch())->default(Approval::PENDING);
+            $table->boolean('verified')->default(false);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname', 50);
+            $table->string('lastname', 50)->nullable();
+            $table->string('username', 15)->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 19)->unique();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->string('otp_code', 6)->unique()->nullable();
+            $table->timestamp('otp_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('status', Approval::fetch())->default(Approval::PENDING);
+            $table->boolean('verified')->default(false);
+            $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('member_id')->nullable();
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
@@ -33,6 +61,7 @@ return new class extends Migration {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
+            $table->foreignId('member_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
