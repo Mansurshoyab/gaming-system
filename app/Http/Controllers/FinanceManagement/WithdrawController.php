ac302031f8c673f2f67dcdc6c80cbbad9c7a5070
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 12:16:33 +0600

    withdraw model created

---
+++ app/Http/Controllers/FinanceManagement/WithdrawController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\FinanceManagement;
+
+use App\Http\Controllers\Controller;
+use App\Models\FinanceManagement\Withdraw;
+use Illuminate\Http\Request;
+
+class WithdrawController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Withdraw $withdraw)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Withdraw $withdraw)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Withdraw $withdraw)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Withdraw $withdraw)
+    {
+        //
+    }
+}
