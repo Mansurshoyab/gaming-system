44b4009356642cb4def84cc3698bd90b012d4ef7
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-12 18:37:36 +0600

    avatars schema designed

--- database/migrations/0001_01_01_000008_create_profiles_table.php
+++ database/migrations/0001_01_01_000008_create_profiles_table.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Enums\GlobalUsage\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('avatars', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->unique();
+            $table->string('title')->unique();
+            $table->string('note')->nullable();
+            $table->enum('status', Status::fetch())->default(Status::PENDING);
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('profiles', function (Blueprint $table) {
@@ -30,8 +34,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('avatars');
         Schema::dropIfExists('profiles');
         Schema::dropIfExists('activities');
