2205ddeae949801c006815e6882955e22dd208ee
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-13 19:31:02 +0600

    city seeder updated

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\GlobalUsage\Status;
+use App\Models\SystemConfiguration\City;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class CitySeeder extends Seeder
 {
@@ -12,6 +15,40 @@ class CitySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        City::truncate();
+
+        $csvPath = Storage::disk('private')->path('locations/cities.csv');
+
+        if (!Storage::disk('private')->exists('locations/cities.csv')) {
+            $this->command->error("CSV file not found at path: $csvPath");
+            return;
+        }
+
+        if (($file = fopen($csvPath, 'r')) !== false) {
+            fgetcsv($file);
+
+            $batchSize = 1000;
+            $dataBatch = [];
+
+            while (($data = fgetcsv($file)) !== false) {
+                $dataBatch[] = [
+                    'country_id'   => $data[5],
+                    'province_id'     => $data[2],
+                    'name'         => $data[1],
+                    'status'       => Status::ENABLE,
+                ];
+
+                if (count($dataBatch) >= $batchSize) {
+                    DB::table('cities')->insert($dataBatch);
+                    $dataBatch = [];
+                }
+            }
+
+            if (!empty($dataBatch)) {
+                DB::table('cities')->insert($dataBatch);
+            }
+
+            fclose($file);
+        }
     }
 }
