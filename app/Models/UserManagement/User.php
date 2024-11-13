ffe38e1690829e26de404432959d83c185ce6f20
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-13 21:47:48 +0600

    user model updated

--- app/Models/UserManagement/User.php
+++ app/Models/UserManagement/User.php
@@ -3,35 +3,33 @@
 namespace App\Models\UserManagement;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\ProfileManagement\Profile;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = [ 'firstname', 'lastname', 'username', 'email', 'phone', 'otp_code', 'password', 'role_id', 'status' ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = [ 'otp_code', 'password', 'verified', 'source', 'remember_token' ];
 
     /**
      * Get the attributes that should be cast.
@@ -42,7 +40,30 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
+            'phone_verified_at' => 'datetime',
+            'otp_verified_at' => 'datetime',
             'password' => 'hashed',
+            'status' => 'string',
+            'source' => 'string',
+            'deleted_at' => 'datetime',
         ];
+    }
+
+    /**
+     * Define an inverse one-to-many relationship with the Role model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role(): BelongsTo {
+        return $this->belongsTo(Role::class, 'role_id', 'id')->withTrashed();
+    }
+
+    /**
+     * Define an inverse one-to-one relationship with the Profile model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function profile(): HasOne {
+        return $this->hasOne(Profile::class, 'user_id', 'id')->withTrashed();
     }
 }
