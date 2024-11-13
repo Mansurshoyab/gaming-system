0c5679ca9096298af4ce3877cd77e15ece1791f3
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-13 20:00:51 +0600

    user seeder updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserManagement\Approval;
+use App\Enums\UserManagement\Source;
+use App\Models\UserManagement\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +16,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::truncate();
+
+        $users = [
+            [ 'firstname' => 'Casino', 'lastname' => 'King' ],
+            [ 'firstname' => 'Shawon', 'lastname' => 'Khan' ],
+            [ 'firstname' => 'Shoaib', 'lastname' => 'Mahmud' ],
+            // [ 'firstname' => '', 'lastname' => '' ],
+        ];
+
+        foreach ($users as $key => $user) {
+            $user['username'] = strtolower($user['firstname'] . '.' . $user['lastname']);
+            $user['email'] = $key === 0 ? 'king@casion.com' : (strtolower($user['firstname']) . '@example.com');
+            $user['email_verified_at'] = now();
+            $user['phone'] = '+8801234567' . (890 + $key);
+            $user['phone_verified_at'] = now();
+            $user['password'] = Hash::make('12345678');
+            $user['role_id'] = 1;
+            $user['status'] = Approval::APPROVED;
+            $user['verified'] = true;
+            $user['source'] = Source::INSTALL;
+            User::create($user);
+        }
     }
 }
