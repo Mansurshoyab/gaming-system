88af7b002009c88132489098284577df0e63f545
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-15 11:36:45 +0600

    greetings message added

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -8,6 +8,43 @@
   @endpush
 
   <section class="row" >
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="d-flex align-items-end row">
+          <div class="col-sm-8">
+            <div class="card-body">
+              <h5 class="card-title text-primary">
+                <span>{{ __('Greetings') }}</span>
+                <strong>{{ auth()->user()->firstname . ' ' . auth()->user()->lastname . '!' }}</strong>
+                <span>🚀</span>
+              </h5>
+              {{-- <p class="mb-2" style="font-size: 1.25rem !important;" >
+                <span>{{ __('Welcome to Casino King.') }}</span>
+                <span>{{ __('Here, you have the power to') }}</span>
+                <span class="fw-bold" >{{ __('oversee, control,') }}</span>
+                <span>{{ __('and enhance performance to new heights! Your passion fuels our achievements, and we\'re thrilled to have you leading this adventure.') }}</span>
+              </p> --}}
+              <p class="mb-2" style="font-size: 1rem !important;">
+                <span>{{ __('Welcome to Casino King!') }}</span>
+                <span>{{ __('Take charge to') }}</span>
+                <span class="fw-bold">{{ __('oversee, control, and elevate performance!') }}</span>
+                <span>{{ __('Your passion drives our success as we reach new heights together.') }}</span>
+                <span>{{ __('We\'re thrilled to have you lead this exciting journey at Casino King, where each decision makes a difference.') }}</span>
+            </p>
+
+            </div>
+          </div>
+          <div class="col-sm-4 text-center text-sm-left">
+            <div class="card-body pb-0 px-0 px-md-4">
+              <img src="{{ asset('images/others/man-with-laptop-light.png') }}" height="120" alt="View Badge User" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="row" >
     <div class="col-sm-12" >
       <div class="card" >
         <div class="card-body" >
