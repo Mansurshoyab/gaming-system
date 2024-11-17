bf2007a20e378ca6b023b40d05cb8dec2645de20
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-17 14:14:10 +0600

    default members created

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserManagement\Approval;
+use App\Enums\UserManagement\Source;
+use App\Models\UserManagement\Member;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class MemberSeeder extends Seeder
 {
@@ -12,6 +16,27 @@ class MemberSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Member::truncate();
+
+        $members = [
+            [ 'firstname' => 'Casino', 'lastname' => 'King' ],
+            [ 'firstname' => 'Uthoai', 'lastname' => 'Marma' ],
+            [ 'firstname' => 'Shawon', 'lastname' => 'Khan' ],
+            [ 'firstname' => 'Shoiab', 'lastname' => 'Mahmud' ],
+            // [ 'firstname' => '', 'lastname' => '' ],
+        ];
+
+        foreach ($members as $key => $member) {
+            $member['username'] = strtolower($member['firstname'] . '.' . $member['lastname']);
+            $member['email'] = $key === 0 ? 'king@casino.com' : strtolower($member['firstname']) . '@example.com';
+            $member['email_verified_at'] = now();
+            $member['phone'] = '+8801234567' . (890 + $key);
+            $member['phone_verified_at'] = now();
+            $member['password'] = Hash::make('12345678');
+            $member['status'] = Approval::APPROVED;
+            $member['verified'] = true;
+            $member['source'] = Source::INSTALL;
+            Member::create($member);
+        }
     }
 }
