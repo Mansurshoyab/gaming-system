0ec73ad6c79c82cb43e206b60eab62e53b0a39a9
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-17 13:53:49 +0600

    default genres created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             CountrySeeder::class,
             ProvinceSeeder::class,
             CitySeeder::class,
+            GenreSeeder::class,
         ]);
     }
 }
--- database/seeders/GenreSeeder.php
+++ database/seeders/GenreSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\GlobalUsage\Status;
+use App\Models\GameManagement\Genre;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class GenreSeeder extends Seeder
 {
@@ -12,6 +14,24 @@ class GenreSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Genre::truncate();
+
+        $genres = [
+            ['title' => 'Card', 'description' => 'Games focused on skill, strategy, or luck using cards, like poker or solitaire.'],
+            ['title' => 'Casino', 'description' => 'Games simulating casino experiences, including slots, poker, and roulette, often with betting elements.'],
+            ['title' => 'Multiplayer', 'description' => 'Games designed for interaction with other players, either competitively or cooperatively.'],
+            ['title' => 'Board', 'description' => 'Digital versions of traditional board games like chess or Ludo, emphasizing turn-based, strategic play.'],
+            ['title' => 'Casual', 'description' => 'Easy-to-learn games with simple mechanics, ideal for short sessions and broad appeal.'],
+            ['title' => 'Simulation', 'description' => 'Games that replicate real-life activities or environments, offering an immersive, realistic experience.'],
+            ['title' => 'Gambling', 'description' => 'Games of chance involving bets or stakes, designed for thrill and risk.'],
+            ['title' => 'Prediction', 'description' => 'Games focused on guessing or forecasting outcomes, relying on probability and timing.'],
+            ['title' => 'Table', 'description' => 'Classic casino-style table games, like blackjack or roulette, often focused on betting and odds.'],
+        ];
+
+        foreach ($genres as $key => $genre) {
+            $genre['slug'] = Str::slug($genre['title']);
+            $genre['status'] = Status::ENABLE;
+            Genre::create($genre);
+        }
     }
 }
