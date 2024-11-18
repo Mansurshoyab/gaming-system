4ac96cde92f3c84da8b1547bbbdbc5344d063099
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-18 18:34:34 +0600

    select component created

---
+++ app/View/Components/Elements/Select.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\Elements;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Select extends Component
+{
+    public $label, $name, $options, $value, $required, $disable;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($label = null, $name, $options = [], $value = null, $required = false, $disable = false)
+    {
+        $this->label = __($label);
+        $this->name = __($name);
+        $this->options = is_array($options) ? $options : [];
+        $this->value = __($value);
+        $this->required = __($required);
+        $this->disable = __($disable);
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.elements.select');
+    }
+}
