c1b2fd4a4786842d5ad2d1e6002fb37a6b750cb2
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-20 00:49:02 +0600

    status functions added

--- app/Library/Functions.php
+++ app/Library/Functions.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Enums\GlobalUsage\Status;
+use App\Enums\UserManagement\Approval;
+
 if (!function_exists('version')) {
     /**
      * Get system build version.
@@ -35,5 +38,37 @@ if (!function_exists('fullname')) {
         } catch (Exception $e) {
             return 'Not Available';
         }
+    }
+}
+
+if (!function_exists('status')) {
+    /**
+     * Get status value.
+     *
+     * @param  string  $value
+     * @return string|null
+     */
+    function status($value) {
+        $status = Status::fetch();
+        if (in_array($value, $status)) {
+            return $value;
+        }
+        return null;
+    }
+}
+
+if (!function_exists('approval')) {
+    /**
+     * Get approval value.
+     *
+     * @param  string  $value
+     * @return string|null
+     */
+    function approval($value) {
+        $approval = Approval::fetch();
+        if (in_array($value, $approval)) {
+            return $value;
+        }
+        return null;
     }
 }
