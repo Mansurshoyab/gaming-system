6e6de2499c2561537c29104bf344ac68a96203a3
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-21 18:37:16 +0600

    default methods seeded

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\GlobalUsage\Status;
+use App\Models\FinanceManagement\Account;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,22 @@ class AccountSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Account::truncate();
+
+        $accounts = [
+            [ 'acc_name' => 'Bank' ],
+            [ 'acc_name' => 'Bkash' ],
+            [ 'acc_name' => 'Nagad' ],
+            [ 'acc_name' => 'Rocket' ],
+            [ 'acc_name' => 'Upay' ],
+            // [ 'acc_name' => '' ],
+        ];
+
+        foreach ($accounts as $key => $account) {
+            $account['acc_code'] = 'ACC00' . (0 + $key);
+            $account['acc_no'] = $key === 0 ? '1234567890' : ('0123456789' . 0 + $key);
+            $account['status'] = Status::ENABLE;
+            Account::create($account);
+        }
     }
 }
