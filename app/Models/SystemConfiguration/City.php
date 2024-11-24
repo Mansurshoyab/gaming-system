8b4feac2e3a1eca557cc427ccea1db6559e54326
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-24 11:43:34 +0600

    city model updated

--- app/Models/SystemConfiguration/City.php
+++ app/Models/SystemConfiguration/City.php
@@ -3,21 +3,28 @@
 namespace App\Models\SystemConfiguration;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
-    protected $fillable =[
-        'country_id',
-        'province_id',
-        'name',
-        'status',
-    ];
+    use SoftDeletes;
 
-    protected function casts() :array 
+    protected $fillable =[ 'country_id', 'province_id', 'name', 'status' ];
+
+    protected function casts() :array
     {
         return [
             'status' => 'string',
             'deleted_at' => 'datetime',
         ];
+    }
+
+    public function country(): BelongsTo {
+        return $this->belongsTo(Country::class, 'country_id', 'id')->withTrashed();
+    }
+
+    public function province(): BelongsTo {
+        return $this->belongsTo(Province::class, 'province', 'id')->withTrashed();
     }
 }
