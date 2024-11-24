0baf8903494226070fab1e42363aaec860bc8790
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-24 13:03:10 +0600

    transaction model updated

--- app/Models/FinanceManagement/Transaction.php
+++ app/Models/FinanceManagement/Transaction.php
@@ -2,27 +2,35 @@
 
 namespace App\Models\FinanceManagement;
 
+use App\Models\UserManagement\Member;
+use App\Models\UserManagement\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    protected $fillable = [
-        'user_id',
-        'member_id',
-        'account_id',
-        'trx_id',
-        'type',
-        'debit',
-        'credit',
-        'amount',
-        'note',
-    ];
+    use SoftDeletes;
 
-    protected function casts() :array 
+    protected $fillable = [ 'user_id', 'member_id', 'account_id', 'trx_id', 'type', 'debit', 'credit', 'amount', 'note' ];
+
+    protected function casts() :array
     {
         return [
             'type' => 'string',
             'deleted_at' => 'datetime',
         ];
+    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id', 'id')->withTrashed();
+    }
+
+    public function member(): BelongsTo {
+        return $this->belongsTo(Member::class, 'member_id', 'id')->withTrashed();
+    }
+
+    public function account(): BelongsTo {
+        return $this->belongsTo(Account::class, 'account_id', 'id')->withTrashed();
     }
 }
