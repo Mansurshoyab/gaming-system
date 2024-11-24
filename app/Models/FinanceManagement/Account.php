e59cd1758d26652fe3391f8c0fcf9eceadefdc80
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-24 13:00:09 +0600

    account model updated

--- app/Models/FinanceManagement/Account.php
+++ app/Models/FinanceManagement/Account.php
@@ -2,7 +2,10 @@
 
 namespace App\Models\FinanceManagement;
 
+use App\Models\ProfileManagement\Payout;
+use App\Models\UserManagement\Member;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
@@ -17,5 +20,21 @@ class Account extends Model
             'status' => 'string',
             'deleted_at' => 'datetime',
         ];
+    }
+
+    public function payouts(): HasMany {
+        return $this->hasMany(Payout::class, 'account_id', 'id')->withTrashed();
+    }
+
+    public function deposits(): HasMany {
+        return $this->hasMany(Deposit::class, 'account_id', 'id')->withTrashed();
+    }
+
+    public function withdraws(): HasMany {
+        return $this->hasMany(Withdraw::class, 'account_id', 'id')->withTrashed();
+    }
+
+    public function transactions(): HasMany {
+        return $this->hasMany(Transaction::class, 'account_id', 'id')->withTrashed();
     }
 }
