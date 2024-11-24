3fcc5976e13d1b547d8e058565ff9e0c1f1b9a72
Muhammad Nasir Uddin Khan Shawon <cupid.srk786@gmail.com>
2024-11-24 12:54:20 +0600

    deposit model updated

--- app/Models/FinanceManagement/Deposit.php
+++ app/Models/FinanceManagement/Deposit.php
@@ -2,24 +2,30 @@
 
 namespace App\Models\FinanceManagement;
 
+use App\Models\UserManagement\Member;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Deposit extends Model
 {
-    protected $fillable = [
-        'member_id',
-        'account_id',
-        'trx_id',
-        'amount',
-        'status',
-        'note',
-    ];
+    use SoftDeletes;
 
-    protected function casts() :array 
+    protected $fillable = [ 'member_id', 'account_id', 'trx_id', 'amount', 'status', 'note' ];
+
+    protected function casts() :array
     {
         return [
             'status' => 'string',
             'deleted_at' => 'datetime',
         ];
+    }
+
+    public function member(): BelongsTo {
+        return $this->belongsTo(Member::class, 'member_id', 'id')->withTrashed();
+    }
+
+    public function method(): BelongsTo {
+        return $this->belongsTo(Account::class, 'account_id', 'id')->withTrashed();
     }
 }
