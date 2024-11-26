5b59cef5ebdc9490bcc8b1519870fe12a6afd93f
shoyab <abumansurshoyab@gmail.com>
2024-11-26 12:53:18 +0600

    General Change

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -37,7 +37,7 @@ class ProfileController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Profile Display',
-            'data' => $data
+            'data' => $data 
         ]);
     }
 
